bf4e1c831fbf9a82afcd6d3192e62ce715026b98
Kerem TÜRKER <social.turker@gmail.com>
2021-11-12 15:10:11 +0300

    First Commit

---
+++ app/src/main/java/com/keremturker/behero/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.keremturker.behero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
